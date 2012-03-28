fce7d435f8ce3817a345859a8b684beaf20dacdd
Daniel Alvarenga <alvarenga_daniel@hotmail.com>
2012-03-28 11:15:25 -0300

    atuzalização dos testes com fb-php-sdk

--- classes/facebook.php
+++ classes/facebook.php
@@ -1,5 +1,5 @@
 <?php
-require_once("../facebook-sdk-php/facebook.php");
+require_once("../facebook-php-sdk/src/facebook.php");
 	$config = array();
 	$config ["appId"] = "233715530059546";
 	$config["secret"] = "0fa65b36e29b5ba8f774827028f67317";
