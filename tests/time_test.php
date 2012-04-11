6bb729618930877240883f058576c137f7f4232a
marcelobandeira <marcelob100@hotmail.com>
2012-04-11 11:18:49 -0300

    Testes antigos da classe time, afora pertencente à classe Classificacao

--- tests/time_test.php
+++ tests/time_test.php
@@ -1,1 +1,63 @@
 <?php
+
+require_once('simpletest/autorun.php');
+require_once('../ChutaBolao/test/Time.php');
+
+class TestOfTime extends UnitTestCase {
+  
+	function testaNumeroDeDerrotas(){
+		$time = new Time();
+		$this->assertEqual($time->setDerrotas(), 1);
+	}
+	
+	function testaNumeroDePontos(){
+		$time = new Time();
+		$this->assertEqual($time->setPontos(3), 3); 
+	}
+	
+	function testaNumeroDeVitorias(){
+		$time = new Time();
+		$this->assertEqual($time->setVitorias(), 1);
+	}
+	
+	function testaNumeroDeEmpates(){
+		$time = new Time();
+		$this->assertEqual($time->setEmpates(), 1);
+	}
+	
+	function testaNumeroDeGolsPro(){
+		$time = new Time();
+		$this->assertEqual($time->setGolsPro(3), 3);
+	}
+	
+	function testaNumeroDeGolsContra(){
+		$time = new Time();
+		$this->assertEqual($time->setGolsContra(2), 2);
+	}
+	
+	function testaSaldoDeGols(){
+		$time = new Time();
+		$time->setGolsPro(5);
+		$time->setGolsContra(2);
+		$this->assertEqual($time->calculaSaldoDeGola(), 3);
+	}
+	
+	function testaPontos1(){
+		$time = new Time();
+		$time->setVitorias();
+		$this->assertEqual($time->calculaPontos(), 3);
+	}
+	
+	function testaPontos2(){
+		$time = new Time();
+		$time->setEmpates();
+		$this->assertEqual($time->calculaPontos(), 1);
+	}
+	
+	function testaPontos3(){
+		$time = new Time();
+		$time->setDerrotas();
+		$this->assertEqual($time->calculaPontos(), 0);
+	}
+}
+?>
