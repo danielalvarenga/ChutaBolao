cede8648bd0fe56027106e86a3b0f4deed183f4f
Daniel Alvarenga <alvarenga_daniel@hotmail.com>
2012-05-16 01:25:28 -0300

    trocando de projeto.

--- includes/convites.php
+++
@@ -1,4 +0,0 @@
-﻿
-<h1>Dreamweaver</h1>
-<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
-<p> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum Lorem.</p>
