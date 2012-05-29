cfc8c6428739f39c2f0aa6e15c968277bdb86a28
Daniel Alvarenga <alvarenga_daniel@hotmail.com>
2012-05-28 22:38:17 -0300

    recolocar time.

--- classes/Time2.php
+++ classes/Time2.php
@@ -1,1 +1,47 @@
 <?php
+
+use Doctrine\Common\Collections\ArrayCollection;
+
+
+/** @Entity */
+class time{
+	
+	/** @Id @Column(type="integer", name="id") @GeneratedValue */
+	private $codTime;
+
+	/** @Column(type="string") */
+	private $nomeTime;
+	
+	/**
+	 * @OneToMany(targetEntity="RendimentoTime", mappedBy="time", cascade={"persist"})
+	 * @var RendimentoTime[]
+	 */
+	protected $rendimentosCampeonatos;
+
+	function __construct($nomeTime){
+		$this->nomeTime = strtoupper($nomeTime);
+		$this->rendimentosCampeonatos = new ArrayCollection();
+	}
+
+	function getCodTime(){
+		return $this->codTime;
+	}
+
+	function setNomeTime($nomeTime){
+		$this->nomeTime = strtoupper($nomeTime);
+	}
+
+	function getNomeTime(){
+		return $this->nomeTime;
+	}
+	
+	function getRendimentosCampeonatos(){
+		return $this->rendimentosCampeonatos;
+	}
+	
+	function adicionaRendimentosCampeonatos($rendimentoTime){
+		$this->rendimentosCampeonatos[] = $rendimentoTime;
+	}
+
+}
+?>
