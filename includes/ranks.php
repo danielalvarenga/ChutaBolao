6a4d450ed201756a42f9974b1c43d1285a6f7310
Daniel Santos <daniel.jago@hotmail.com>
2012-06-18 21:17:52 -0300

    atualiacao daniel

--- includes/ranks.php
+++ includes/ranks.php
@@ -1,96 +1,261 @@
-﻿<?php 
-$dql = "SELECT u FROM Usuario u  ORDER BY u.pontosGeral DESC";
-$query= $entityManager->createQuery($dql);
-$usuarios= $query->getResult();
-    if($usuarios<>NULL){
-       echo '<div id="Mural">
-		    	<span class="titulo">Classificacao Geral</span>
-	 		    	<span id="compartilharMural">
-					<a class="compartilhar" href="" target="_blank">compartilhar</a>
-		    </span>';
+﻿<?php
+$conn = $entityManager->getConnection();
+$conn->beginTransaction();
+try{
 
+$usuario = $entityManager->find("Usuario", $user_id);
+ 
+echo "<table id='tabela' align='center' cellspacing=0>
+			<td id='aposta'align='center' colspan='6'>
+				Ranking Classificacao Geral
+			</td class=\"coluna\">
+		<tr align='center'class=\"linha\">
+				<td>
+					Usuario Teste
+				</td class=\"coluna\">
+				<td >
+					Classificacao Geral
+				</td>
+				<td >
+					Classificacao Medalhas
+				</td class=\"coluna\">
+				<td >
+					<img  src='..\ChutaBolao\imagens\premios\medalha-ouro.png'>
+				</td class=\"coluna\">
+				<td >
+					<img  src='..\ChutaBolao\imagens\premios\medalha-ouro.png'>
+				</tdclass=\"coluna\">
+				<td >
+					<img  src='..\ChutaBolao\imagens\premios\medalha-ouro.png'>
+				</td class=\"coluna\">
+		
+		</tr><br>	
+    	   
+		<tr align='center' bgcolor='lime'>
+		   	<font size='4'>
+		    	<td>
+		    		Sua Classificacao
+		    	</td>
+		    	<td>"
+		    		.$usuario->getClassificacaoGeral()."
+		    	</td>
+		    	<td>	
+		    		Teste
+		    	</td>
+		    	<td>"
+		    		.$usuario->getPontosGeral()."
+		    	</td>
+		    	<td>"
+		    		.$usuario->getPrimeiroNomeUsuario()."
+		    	</td>
+		    	<td>"
+		    		.$usuario->getSegundoNomeUsuario()."
+		    	</td>
+		    </font>
+		</tr>";
+ 
+$dql = "SELECT u FROM Usuario u ORDER BY u.classificacaoGeral ";
+$query= $entityManager->createQuery($dql);
+$query->setMaxResults(10);
+$usuarios= $query->getResult();
+    
+       
+       if($usuarios<>NULL){	
 		    foreach ($usuarios as $usuario){
 		    	if ($user_id==$usuario->getIdUsuario()) {
-		    		echo "<table border='0' celpadding='0' celspacing='0' style='display:inline-block;
-		    		         *display:inline; _display:inline;' height='58'><tr><td class='texto'>
-		    		         <span id='pontos' >Sua Posicao: ".$usuario->getPrimeiroNomeUsuario().
-		    		     " pontuacao de medalhas ".$usuario->getPontosGeral()."</span></td ></tr></table><br>";
+		    	echo "<tr align='center'  bgcolor='lime'>
+		    				<font size='4'>
+		    					<td>"
+		    						.$usuario->getIdUsuario()."
+		    					</td>
+		    					<td>"
+		    						.$usuario->getClassificacaoGeral()."
+		    					</td>
+		    					<td>
+		    						Teste
+		    					</td>
+		    					<td>"
+		    						.$usuario->getPontosGeral()."
+		    					</td>
+		    					<td>"
+		    						.$usuario->getPrimeiroNomeUsuario()."
+		    					</td>
+		    					<td>"
+		    						.$usuario->getSegundoNomeUsuario()."
+		    					</td>
+		    				</font>
+		    		</tr>";
 		    	}
 		    	else{
-		            echo "<table border='0' celpadding='0' celspacing='0' style='display:inline-block;
-		                     *display:inline; _display:inline;' height='58'><tr><td class='texto'>
-		                     <span id='chuteiras' >".$usuario->getPrimeiroNomeUsuario().
-		                 " pontuacao de medalhas ".$usuario->getPontosGeral()."</span></td ></tr></table><br>";
-		    	}
+		    		echo "<tr align='center' bgcolor='#FFA500'>
+							<font size='4'>		    		
+		    					<td>"
+		    						.$usuario->getIdUsuario()."
+		    					</td>
+		    					<td>"
+		    						.$usuario->getClassificacaoGeral()."
+		    					</td>
+		    					<td>
+		    						Teste
+		    					</td>
+		    					<td>"
+		    						.$usuario->getPontosGeral()."
+		    					</td>
+		    					<td>"
+		    						.$usuario->getPrimeiroNomeUsuario()."
+		    					</td>
+		    					<td>"
+		    						.$usuario->getSegundoNomeUsuario()."
+		    					</td>
+		    				</font>
+		    			</tr>";
+		    		 
+			        }
 		    }
-	  echo '</div>';
-  }
-	$dql = "SELECT p FROM PremiosUsuario p  GROUP BY p.pontosMedalhas ORDER BY
-	 p.pontosMedalhas  DESC";
-	$query= $entityManager->createQuery($dql);
-	$premios= $query->getResult();
-	if($premios<>NULL){
-		$contador=0;
-		echo '<div id="Mural">
-					
-		 			<span class="titulo">Classificacao Geral Medalhas</span>
-		 			    <span id="compartilharMural">
-						<a class="compartilhar" href="" target="_blank">compartilhar</a>
-			        </span><br>';
-		foreach ($premios as $premio){
-			$contador++;
-			if ($user_id==$premio->getUsuario()->getIdUsuario()) {
-				echo "<table border='0' celpadding='0' celspacing='0' style='display:inline-block; *display:inline; _display:inline;' height='58'>
-			             <tr><td><span id='pontos' >classificacao".$contador." ".$premio->getUsuario()->getIdUsuario().
-			                "<br> pontos medalhas ".$premio->getPontosMedalhas()."<br> medalhas de ouro ".$premio->getMedalhasOuro().
-			                "<br> medalhas de prata".$premio->getMedalhasPrata().
-			             "<br>medalhas de bronze".$premio->getMedalhasBronze().
-				     "</span></td ></tr></table><br>";
-			}
-			else{
-				echo "<br> classificacao".$contador." ".$premio->getUsuario()->getIdUsuario().
-				        "<br> pontos medalhas ".$premio->getPontosMedalhas().
-			                "<br> medalhas de ouro ".$premio->getMedalhasOuro().
-			                "<br> medalhas de prata".$premio->getMedalhasPrata().
-			            "<br>medalhas de bronze".$premio->getMedalhasBronze()."<br>";
-			}
-		}
-	}
-		echo '</div>';
-	
-/*$dql = "SELECT c FROM Campeonato c WHERE c.status='ativo' ";
+echo "</table>";  
+    }
+	//classificao geral ok
+$dql = "SELECT c FROM Campeonato c WHERE c.status='ativo' ";
 $query= $entityManager->createQuery($dql);
 $campeonatos= $query->getResult();
 
 if($campeonatos<>NULL){
-
 	foreach ($campeonatos as $campeonato)	{
-$dql = "SELECT p FROM PremiosUsuario p WHERE p.campeonato=".$campeonato->getCodCampeonato();
-			$query = $entityManager->createQuery($dql);
-			$premiosUsuario = $query->getResult();
- echo '<div id="Mural"><span class="titulo">'.$campeonato->getNomeCampeonato().' '.$campeonato->getAnoCampeonato().'</span>';
-if ($premiosUsuario<>NULL) {
-	echo '
+			$dql = "SELECT p FROM PremiosUsuario p WHERE p.campeonato=".$campeonato->getCodCampeonato() ;
+			$query= $entityManager->createQuery($dql);
+			$query->setMaxResults(10);
+			$premiosUsuario= $query->getResult();
 				
-	 			<span id="compartilharMural">
-					<a class="compartilhar" href="" target="_blank">compartilhar</a>
-		        </span>
-		    
-	';	
+if ($premiosUsuario<>NULL) {
+	 $premioUsuario=$entityManager->find("PremiosUsuario", array(
+								"campeonato" =>	$campeonato->getCodCampeonato(),
+								"usuario" => $user_id
+								));
+	 	
+	echo "<table id='tabela' align='center' cellspacing=0 >
+				<td id='aposta'align='center' colspan='6'>
+					Ranking ".$campeonato->getNomeCampeonato()." ".$campeonato->getAnoCampeonato()."
+				</td class=\"coluna\">
+			<tr align='center'class=\"linha\" >
+					<td >
+						Usuario Teste
+					</tdclass=\"coluna\">
+					<td>
+						Classificacao Campeonato
+					</td class=\"coluna\">
+					<td >
+						Classificacao Medalhas
+					</td class=\"coluna\">
+					<td>
+						<img  src='..\ChutaBolao\imagens\premios\medalha-ouro.png'>
+					</td class=\"coluna\">
+					<td >
+						<img  src='..\ChutaBolao\imagens\premios\medalha-ouro.png'>
+					</td class=\"coluna\">
+					<td >
+						<img  src='..\ChutaBolao\imagens\premios\medalha-ouro.png'>
+					</td class=\"coluna\">
+			
+			</tr><br>	
+	    	   
+			<tr align='center' bgcolor='lime'>
+			   	<font size='4'>
+			    	<td >
+			    		Sua Classificacao
+			    	</td>
+			    	<td >"
+						.$premioUsuario->getClassificacaoCampeonato()."
+			    	</td>
+			    	<td >"	
+			    		.$premioUsuario->getClassificacaoMedalhas()."
+			    	</td>
+			    	<td >"
+						.$premioUsuario->getMedalhasOuro()."
+			    	</td>
+			    	<td >"
+						.$premioUsuario->getMedalhasPrata()."
+			    	</td>
+			    	<td >"
+						.$premioUsuario->getMedalhasBronze()."
+			    	</td>
+			    </font>
+			</tr>";
+	
+	
 foreach ($premiosUsuario as $premioUsuario)	{
 	if ($user_id==$premioUsuario->getUsuario()->getIdUsuario()) {
-		echo "<table border='0' celpadding='0' celspacing='0' style='display:inline-block; *display:inline; _display:inline;' height='58'>
-			        <tr><td><span id='pontos' >".$premioUsuario->getUsuario()->getIdUsuario()." pontucacao ".$premioUsuario->getPontosCampeonato()."</span></td ></tr></table><br>";
-	}
+			echo "<tr align='center' bgcolor='lime'>
+							<font size='4'>		    		
+		    					<td>"
+		    						.$premioUsuario->getUsuario()->getIdUsuario()."
+		    					</td>
+		    					<td>"
+									.$premioUsuario->getClassificacaoCampeonato()."
+			    				</td>
+			    				<td>"	
+			    					.$premioUsuario->getClassificacaoMedalhas()."
+			    				</td>
+			    				<td>"
+									.$premioUsuario->getMedalhasOuro()."
+			    				</td>
+			    				<td>"
+									.$premioUsuario->getMedalhasPrata()."
+			    				</td>
+			    				<td>"
+									.$premioUsuario->getMedalhasBronze()."
+			    				</td>
+			    			</font>
+		    	</tr>";
+		    		 
+			        }
+		    
+
 else{
-echo " ".$premioUsuario->getUsuario()->getIdUsuario()."<br>";
-}
+	echo "<tr align='center' bgcolor='#FFA500'>
+								<font size='4'>		    		
+			    					<td>"
+										.$premioUsuario->getUsuario()->getIdUsuario()."
+			    					</td>
+			    					<td>"
+										.$premioUsuario->getClassificacaoCampeonato()."
+			    					</td>
+			    					<td>"
+			    						.$premioUsuario->getClassificacaoMedalhas()."
+			    					</td>
+			    					<td>"
+										.$premioUsuario->getMedalhasOuro()."
+			    					</td>
+			    					<td>"
+										.$premioUsuario->getMedalhasPrata()."
+			    					</td>
+			    					<td>"
+										.$premioUsuario->getMedalhasBronze()."
+			    					</td>
+			    				</font>
+		 </tr>";
+	 
+									}
 
-}
+							}
 
-}
-echo "</div>";
-}
-}
-}*/
-?>
+					}
+			}
+	}
+echo "</table>";
+$conn->commit();
+} catch(Exception $e) {
+	$conn->rollback();
+
+	echo
+		"<p align='center'>
+		<table id='tabela'>
+		<tr class=\"linha\">
+		<td class=\"coluna\">
+		<p align='center'>Não existem dados a serem exibidos. Volte amanhã para conferir.</p>
+		</td>
+		</tr>
+		</table>";
+	}
+	$conn->close();
+
+       ?>
