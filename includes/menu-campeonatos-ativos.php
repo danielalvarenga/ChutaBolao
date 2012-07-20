307ec9de72c2af71c6cd0bee6a0e345e505021ed
Daniel Alvarenga <alvarenga_daniel@hotmail.com>
2012-07-20 00:12:39 -0300

    arquivos de menu.

---
+++ includes/menu-campeonatos-ativos.php
@@ -0,0 +1,37 @@
+<div id="menuCampeonatos">
+<h3 class="titulo"><?php echo $tituloMenu;?></h3>
+		<form action="" method="POST">
+			<!-- <input type="hidden" name="geral" value="geral"> -->
+			<button class="<?php echo $classeGeral;?>" type="submit" name="geral" value="geral">
+				<?php echo $todosMenu;?>
+			</button><br/>
+		</form>
+		<div class="divisoriaMenuCampeonatos"></div>
+
+		<?php
+		$dql = "SELECT c FROM Campeonato c WHERE c.status = 'ativo' ORDER BY c.codCampeonato ASC";
+		$query = $entityManager->createQuery($dql);
+		$campeonatos = $query->getResult();
+		foreach($campeonatos as $campeonato) {
+			if($campeonato instanceof $campeonato){
+				$classe = 'campeonatoInativo';
+				if(isset($_POST['campeonatoMenu'])){
+					if($_POST['campeonatoMenu'] == $campeonato->getCodCampeonato()){
+						$classe = 'campeonatoAtivo';
+					}
+				}
+				?>
+				<form name="<?php echo 'form'.$campeonato->getCodCampeonato();?>" action="" method="POST">
+					<button class="<?php echo $classe;?>" type="submit" name="campeonatoMenu" value="<?php echo $campeonato->getCodCampeonato();?>">
+						<img class="logoCampeonato" src="<?php echo $campeonato->getUrlLogo();?>">
+						<span class="nomeCampeonato">
+							<?php echo $campeonato->getNomeCampeonato();?>
+						</span>
+					</button>
+				</form>
+				<?php
+			}
+		}
+		
+?>
+</div>
---
+++ includes/menu-campeonatos-inativos.php
@@ -0,0 +1,27 @@
+<div id="menuCampeonatos">
+<?php
+$dql = "SELECT c FROM Campeonato c WHERE c.status = 'finalizado' ORDER BY c.codCampeonato ASC";
+$campeonatos = consultaDql($dql);
+foreach($campeonatos as $campeonato) {
+	if($campeonato instanceof $campeonato){
+		$classe = 'campeonatoInativo';
+		if(isset($_POST['campeonatoMenu'])){
+			if($_POST['campeonatoMenu'] == $campeonato->getCodCampeonato()){
+				$classe = 'campeonatoAtivo';
+			}
+		}
+		?>
+				<form name="<?php echo 'form'.$campeonato->getCodCampeonato();?>" action="" method="POST">
+					<button class="<?php echo $classe;?>" type="submit" name="campeonatoMenu" value="<?php echo $campeonato->getCodCampeonato();?>">
+					<img class="logoCampeonato" src="<?php echo $campeonato->getUrlLogo();?>">
+						<span class="nomeCampeonato">
+							<?php echo $campeonato->getNomeCampeonato();?><br/><font size="1">encerrado</font>
+						</span>
+					</button>
+				</form>
+				<?php
+			}
+		}
+		
+?>
+</div>
